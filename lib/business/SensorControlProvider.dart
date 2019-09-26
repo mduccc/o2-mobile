178d1366ddf2475f74095d5bd9caa3d65cd24fff
mduccc <duc2521997@gmail.com>
2019-09-27 00:34:03 +0700

    Build Sensor Business

---
+++ lib/business/SensorControlProvider.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:o2_mobile/business/EndPoint.dart';
+import 'package:o2_mobile/models/SensorModel.dart';
+import 'package:http/http.dart' as http;
+
+import 'DatabaseProvider.dart';
+
+class SensorControlProvider {
+  http.Client _client = http.Client();
+
+  Future<State> state() async {
+    try {
+      await databaseProvider.openOrCreate();
+
+      http.Response response = await this._client.post(EndPoint.sensorState,
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: json.encode({'token': databaseProvider.getToken()}));
+      return State.fromJson(json.decode(response.body));
+    } catch (err) {
+      print(err);
+      return null;
+    }
+  }
+
+  Future<Switch> _switch(String sensor_name, String _switch) async {
+    try {
+      await databaseProvider.openOrCreate();
+
+      http.Response response = await this._client.post(EndPoint.sensorSwitch,
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: json.encode(
+              {'token': databaseProvider.getToken(), 'sensor_name': _switch}));
+      return Switch.fromJson(json.decode(response.body));
+    } catch (err) {
+      print(err);
+      return null;
+    }
+  }
+}
