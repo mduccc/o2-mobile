f0a197ffcae75687dd902ba4ed16058fe820fbb2
mduccc <duc2521997@gmail.com>
2019-09-28 16:26:06 +0700

    Fix Bottom Title Bar Display Invalid

--- lib/ui/fragment/Chart.dart
+++ lib/ui/fragment/Chart.dart
@@ -101,7 +101,10 @@ class _ChartState extends State<ChartFrag> {
                         newTitle = h + 'h' + m;
                       } else
                         newTitle = valueString;
-                      return newTitle;
+                      if (newTitle.length <= 5)
+                        return newTitle;
+                      else
+                        return '';
                     }),
                 rightTitles: SideTitles(showTitles: false)),
             // List data here
